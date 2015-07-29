c999a1b7ce24c982aac32f2d31c5d281547281ee
Tyler Sheaffer <tyler@calm.com>
2015-07-29 16:51:48 -0700

    delete Demo Proj

--- Pods/Target Support Files/Pods-CLKTooltip-pop/Pods-CLKTooltip-pop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CLKTooltip_pop : NSObject
-@end
-@implementation PodsDummy_Pods_CLKTooltip_pop
-@end
--- Pods/Target Support Files/Pods-CLKTooltip/Pods-CLKTooltip-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CLKTooltip : NSObject
-@end
-@implementation PodsDummy_Pods_CLKTooltip
-@end
